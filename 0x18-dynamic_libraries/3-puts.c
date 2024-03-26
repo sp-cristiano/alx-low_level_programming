5b06dc05eb828cfd7c2abb20c2adfee0ce319db3
Odili Chinedum Christian <odili.official@gmail.com>
2024-03-26 02:50:32 -0700

    all tasks' '

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts - Prints to stdout
+ * @str: hold the string
+ * Description: prints to stdout
+ * Return: Always 0 on success
+ */
+void _puts(char *str);
+
+/**
+ * main - Entry point
+ *
+ * Description: The main function, entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
