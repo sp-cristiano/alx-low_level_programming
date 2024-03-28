32a1d35ffbe226a1519042b3870cbed9d8200866
Odili Chinedum Christian <odili.official@gmail.com>
2024-03-28 07:02:04 -0700

    tasl 3

--- 0x18-dynamic_libraries/101-ccmake_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -shared -o myprintf.so -fPIC test.c
-export LD_PRELOAD=./myprintf.so
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/sp-cristiano/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/temp/libgiga.so
+gcc -shared -o myprintf.so -fPIC test.c
+export LD_PRELOAD=./myprintf.so
